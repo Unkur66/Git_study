ef3a28756901b675186ea4950bbea3b1352c8873
Unkur66 <106479749+Unkur66@users.noreply.github.com>
2024-03-05 19:45:36 +0800

    Update GitTest.java

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello master");
         System.out.println("push hot-fix");
+        System.out.println("pull test");
     }
 }
